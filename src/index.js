dcdb5a0f14e26bca738a8e5e231b15e9f355fcbc
Muray1008 <seniordev961008@gmail.com>
2024-07-23 11:26:52 +0900

    eugene fix: index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Second from "./second";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { Provider } from "react-redux";
